b8e54e03110fdff61a070c3d80ee4934b27ebaf1
paperbenni <paperbenni@gmail.com>
2018-07-16 20:00:35 +0200

    add execution

--- spigot.sh
+++ spigot.sh
@@ -20,3 +20,5 @@ else
 fi 
 
 echo "eula=true" > eula.txt #accept eula
+
+java -Xmx650m -Xms650m -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar spigot.jar
